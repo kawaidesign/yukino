a7cabff481233cfb6ede98a8af409abb92da74ff
kawai <takuyakawai.1989@gmail.com>
2022-08-02 16:19:57 +0900

    update galleryの画像・キャプション追加

--- constants/workList.tsx
+++ constants/workList.tsx
@@ -6,15 +6,258 @@ type workList = {
 
 export const WORKLIST: workList[] = [
   {
-    src: "/images/gallery/29_rocca_skirt.jpg",
-    title: "「ブーケ」/ textile / 2021",
+    src: "/images/gallery/44_yukino_myatelier.jpg",
+    title: "私のアトリエ",
     caption:
-      "説明が入ります。説明が入ります。説明が入ります。説明が入ります。説明が入ります。説明が入ります。説明が入ります。説明が入ります。説明が入ります。",
+      "私の台東デザイナーズビレッジのアトリエをイメージ して作った書き起こした柄です。デザビレに住む 猫の長谷川さんやうちのパグ、テキスタイル道具や部屋番号などをモチーフにぎゅぎゅっと詰め込みました。私のアトリエの壁紙もこちらの柄で作りました。",
   },
   {
-    src: "/images/gallery/29_rocca_skirt.jpg",
-    title: "「ブーケ」/ textile / 2021",
+    src: "/images/gallery/43_yukino_nezou.jpg",
+    title: "nezou cats",
     caption:
-      "説明が入ります。説明が入ります。説明が入ります。説明が入ります。説明が入ります。説明が入ります。説明が入ります。説明が入ります。説明が入ります。",
+      "寝相猫の新しい柄を作りました。猫の日に書かせて いただいた猫ちゃんのイラストをアレンジして、 NEZOUの文字と組み合わせたシュールな柄になって います。男性にも使ってほしくて渋めのイエローに。",
+  },
+  {
+    src: "/images/gallery/42_yukino_miniflower.jpg",
+    title: "mini flower",
+    caption:
+      "小さな花々を散りばめた、roccaの小花柄を作りました。華やかで可愛らしい柄は少しくすんだカラーで大人も子供も使い勝手が良く仕上げました。小花柄を纏って お部屋やコーデが華やかになりますように。",
+  },
+  {
+    src: "/images/gallery/41_yukino_check.jpg",
+    title: "checks",
+    caption:
+      "手書きで描いたチェックを使用した柄です。 懐かしい記憶の中にあるチェックを再現してみました。 シンプルながらも可愛い配色でインテリアや ファッションコーデに活躍してくれそう。",
+  },
+  {
+    src: "/images/gallery/40_yukino_nigaoe.jpg",
+    title: "似顔絵イラスト",
+    caption:
+      "大人気の、「好きな動物と描く似顔絵サービス」のサンプルイラスト。自身と愛犬のチオビタをモチーフにしました。オンラインショップで受付中のほか、似顔絵イベントなども行っています。あなたの好きなもの、なんでものせちゃいます。",
+  },
+  {
+    src: "/images/gallery/39_yukino_pugincats.jpg",
+    title: "Pug in cats",
+    caption:
+      "重い思いにくつろぎ寝転がる猫、猫、猫。その中によく見ると、パグが混ざってる。きっと自分を猫だと思っている、パグと猫たちのイラストです。",
+  },
+  {
+    src: "/images/gallery/38_yukino_yamabiko.jpg",
+    title: "YAMABIKO",
+    caption:
+      "野外イベント用に書き起こした、野外音楽フェスの楽しいフィン行きを表したイラストです。よく見るとイベントを楽しんでいるパグが。自然モチーフをリースのように仕立てたイラストは2色刷りのシルクスクリーンで限定グッズ化しました。",
+  },
+  {
+    src: "/images/gallery/37_yukino_camppug.jpg",
+    title: "CAMP PUG",
+    caption:
+      "野外イベント用に書き起こした、アウトドアな格好をしたパグ。キャンプにやる気満々の格好だけど、どこか着せられている感の拭えない表情がポイント。",
+  },
+  {
+    src: "/images/gallery/36_yukino_borderflower.jpg",
+    title: "ボーダー花柄",
+    caption:
+      "線画で描いた花のイラストを並べて、ボーダー柄にしてみました。ゆるいボーダーラインが優しい印象の柄です。",
+  },
+  {
+    src: "/images/gallery/35_yukino_mopdog.jpg",
+    title: "モップ犬",
+    caption:
+      "モップと、モップみたいな犬が掃除に奮闘する様子を柄にしました。犬のもふもふ感がポイントです。",
+  },
+  {
+    src: "/images/gallery/34_yukino_kiwidot.jpg",
+    title: "キウイドット",
+    caption:
+      "キウイフルーツとキウイバードがわちゃわちゃ転がった、「キウイドット柄」です。爽やかなグリーンが爽やかな柄になっています。",
+  },
+  {
+    src: "/images/gallery/33_yukino_pug.jpg",
+    title: "PUG",
+    caption:
+      "愛犬であるパグ、チオビタからインスピレーションを 受けて作成したイラストです。パグ座り(パグがよくするお姉さん座りのこと)をしているのがポイント。シンプルなラインでパグらしさを表現できたと思います。",
+  },
+  {
+    src: "/images/gallery/32_yukino_rakugakicheck.jpg",
+    title: "rakugaki check",
+    caption:
+      "カラフルな色合いが特徴の様々なラインを、チェック柄に仕立てました。所々に猫が混ざっているのがポイントです。元はカラフルな輪っかのオブジェだったのですが、テキスタイルにしてみたいなあ、と柄におこしてみました。",
+  },
+  {
+    src: "/images/gallery/31_yukino_pugs.jpg",
+    title: "PUGS",
+    caption:
+      "愛犬であるパグ、チオビタからインスピレーションを 受けて作成したテキスタイルです。パグのフォルムは 独特の丸みで愛らしいのです。いろんなポーズのパグを テキスタイルに起こしました。",
+  },
+  {
+    src: "/images/gallery/30_yukino_camome.jpg",
+    title: "camome",
+    caption:
+      "昔から何度も描いてきたカモメの絵を、今回テキスタイル 用に新たに書き起こしました。学生の時に音楽に合わせてカモメが羽ばたく映像を作ったのがきっかけで、真正面から見た気の抜けたカモメのイラストはたびたび私の作品に登場してきました。大人でも使いやすいシックなカラーの柄に仕上げました。",
+  },
+  {
+    src: "/images/gallery/29_rocca_skirt_bouquet.jpg",
+    title: "bouquet / textile",
+    caption:
+      "このブーケ柄は、以前に描いている「bouquet」という イラストをもとに作られています。紙で作ったアクセサ リーシリーズの原画で、黒い背景に白いお花が映える カラーインクで描かれたイラストです。 このブーケイラストはまあるく描いた花が思いのほか 可愛かったので切る前に残しておきました。水彩の グレーをベースにたくさんのブーケを並べて、シックな 色の中にしっかりと可愛らしさが残るテキスタイルに なってくれました。",
+  },
+  {
+    src: "/images/gallery/28_rocca_chiritsumo.jpg",
+    title: "chiritumo",
+    caption:
+      "捨てられなかったイラストの破片で作りました。 作品整理をする際にでてきたそれらは、袋のなかいっぱい にキラキラしていて、このまま柄にしてしまったら面白い なと思いまるで雪が降り積もるようなテキスタイルに生ま れ変わりました。小さな、ゴミになるちりのような存在 だったそれらをキラキラに積らせたその柄を 「chiritumo」(ちりつも)と名付けました。 小さなことの積み重ね、というポジティブなメッセージ も込められた鮮やかなテキスタイルです。",
+  },
+  {
+    src: "/images/gallery/27_rocca_illust_for_socks.jpg",
+    title: "靴下用シルクイラスト",
+    caption: "靴下にプリントするために書き下ろしたワンポイントイラストです。",
+  },
+  {
+    src: "/images/gallery/26_rocca_postcard.jpg",
+    title: "長型ポストカード",
+    caption: "個展「猫と耳飾り展」のDM用に書き下ろしたイラスト。",
+  },
+  {
+    src: "/images/gallery/25_rocca_flowers.jpg",
+    title: "FLOWERS",
+    caption:
+      "この柄は絵本「はなさき山」から影響を受けています。 絵本に出てくる綺麗な花が大好きで、花柄を作ろうと した時に自分でも黒地に綺麗な花を描いてみたくなりました。",
+  },
+  {
+    src: "/images/gallery/24_rocca_wansaka.jpg",
+    title: "wansaka",
+    caption:
+      "小さな犬がわんさか溢れた、わんわんわんさか柄。 ちょっとした落書きが元になって生まれた、なんの 犬種かわからない犬たちがせわしなくわさわさと 駆け回る元気な柄です。",
+  },
+  {
+    src: "/images/gallery/23_rocca_my_accessory.jpg",
+    title: "my accessory",
+    caption:
+      "私の作っているアクセサリーをモチーフにした幾何学柄です。さまざまな模様や色が並び様子が可愛う愛らしく、柄に仕立てました。",
+  },
+  {
+    src: "/images/gallery/22_rocca_ribonborder_aw.jpg",
+    title: "ribbon border",
+    caption:
+      "ボーダーラインをリボンに見立てたシンプルながらも遊び心のある柄です。",
+  },
+  {
+    src: "/images/gallery/21_rocca_bokunchi_a.jpg",
+    title: "ぼくんちA",
+    caption: "ここはぼくんちだぞ、とのびのび暮らす猫のイラストです。",
+  },
+  {
+    src: "/images/gallery/20_rocca_bokunchi.jpg",
+    title: "ぼくんち",
+    caption: "ここはぼくんちだぞ、とのびのび暮らす猫のイラストです。",
+  },
+  {
+    src: "/images/gallery/19_rocca_tower.jpg",
+    title: "tower",
+    caption:
+      "キャットタワーに群がり寛ぐ猫をモチーフにしたイラストです。イラストレーターとして独立する際に最初に描いた、活動の原点でもあるイラストです。",
+  },
+  {
+    src: "/images/gallery/18_rocca_mizutori.jpg",
+    title: "水鳥",
+    caption:
+      "水辺の鳥が好きで、スケッチと一緒に描いていたイラストがとても可愛かったので友人からのイラストオーダーの際に改めて書き起こしました。",
+  },
+  {
+    src: "/images/gallery/17_rocca_toriberry.jpg",
+    title: "鳥ベリー",
+    caption:
+      "ベリーをくわえた、シンプルなラインの鳥のイラストです。イラストレーターとして活動するきっかけになった、友人からのイラストオーダーの際に書き起こしました。",
+  },
+  {
+    src: "/images/gallery/16_rocca_tori.jpg",
+    title: "tori",
+    caption: "さまざまな鳥が行き交う鳥の柄です。",
+  },
+  {
+    src: "/images/gallery/15_rocca_longdog.jpg",
+    title: "長い犬",
+    caption:
+      "体の長い犬たちが辛なった柄です。落書きのように手を動かしていたら生まれた、愛らしい存在です。",
+  },
+  {
+    src: "/images/gallery/14_rocca_nezou_a.jpg",
+    title: "NEZOU A",
+    caption: "変な寝相の猫のイラスト。実家の寝相が変な猫がモデルです。",
+  },
+  {
+    src: "/images/gallery/13_rocca_nezou_b.jpg",
+    title: "NEZOU B",
+    caption: "変な寝相の猫のイラスト。実家の寝相が変な猫がモデルです。",
+  },
+  {
+    src: "/images/gallery/12_rocca_nezoutextile.jpg",
+    title: "NEZOU textile",
+    caption:
+      "変な寝相の猫のイラストを柄にしてしまいました。実家の寝相が変な猫がモデルです。",
+  },
+  {
+    src: "/images/gallery/11_rocca_jam_a.jpg",
+    title: "じゃれるシャム",
+    caption: "チョウチョにじゃれる、ひょうきんな猫のイラストです。",
+  },
+  {
+    src: "/images/gallery/10_rocca_jam_b.jpg",
+    title: "びっくりシャム",
+    caption:
+      "チョウチョにびっくりしてしっぽがボッとなってしまう、どこかひょうきんな猫のイラストです。",
+  },
+  {
+    src: "/images/gallery/09_rocca_ribbon_mug.jpg",
+    title: "ribbon A",
+    caption:
+      "水彩で描いたカラフルなリボン柄。ギフト(贈り物)をテーマに作成しました。",
+  },
+  {
+    src: "/images/gallery/08_rocca_yurinoiro.jpg",
+    title: "花のいろ",
+    caption:
+      "お花屋さんで見た花びらが美しかったので、そのイメージを筆に込めました。",
+  },
+  {
+    src: "/images/gallery/07_rocca_tubomi.jpg",
+    title: "tsubomi",
+    caption:
+      "花の蕾をたくさん並べたイラスト。これから咲くつぼみのワクワクを思いながら描きました。",
+  },
+  {
+    src: "/images/gallery/06_rocca_ribon_2.jpg",
+    title: "ribbon B",
+    caption:
+      "水彩で描いたカラフルなリボン柄。ギフト(贈り物)をテーマに作成しました。",
+  },
+  {
+    src: "/images/gallery/05_rocca_kamome.jpg",
+    title: "カモメ",
+    caption: "優雅に港町で飛ぶたくさんのカモメたちを正面から描きました。",
+  },
+  {
+    src: "/images/gallery/04_rocca_bouquet.jpg",
+    title: "bouquet",
+    caption:
+      "水彩で描いた、ブーケをモチーフにしたイラスト。白い小花が可憐に生き生きと生活に寄り添ってくれますように。",
+  },
+  {
+    src: "/images/gallery/03_rocca_dogs.jpg",
+    title: "小型犬たち",
+    caption:
+      "小型犬が列になって並んでいる、ちょっぴりクスッと笑ってしまう可愛さのある柄です。",
+  },
+  {
+    src: "/images/gallery/02_rocca_getumenlabocard.jpg",
+    title: "getumenlabo card B",
+    caption:
+      "アクセサリーブランドのショップカードように書き下ろしたイラストです。",
+  },
+  {
+    src: "/images/gallery/01_rocca_getumenlabocard2.jpg",
+    title: "getumenlabo card A",
+    caption:
+      "アクセサリーブランドのショップカードように書き下ろしたイラストです。",
   },
 ];
